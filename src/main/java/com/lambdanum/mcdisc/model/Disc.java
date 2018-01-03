1d0368cbb8c28dda15228ad50d9bdd13e2c9af52
KEOTL <kento.lauzon@ligature.ca>
2018-01-03 10:30:01 -0500

    Removed unused fields in Disc model

--- src/main/java/com/lambdanum/mcdisc/model/Disc.java
+++ src/main/java/com/lambdanum/mcdisc/model/Disc.java
@@ -4,9 +4,5 @@ public class Disc {
 
     public int id;
     public String minecraftId;
-    public String url;
-    public String name;
-    public String artist;
-    public boolean released;
     public String soundId;
 }
